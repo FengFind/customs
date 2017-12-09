3863a1f265d4ab65433ee3aef253142c19272d9a
nravel <2357921020@qq.com>
2017-12-09 08:40:45 +0800

    update config.php

--- application/config.php
+++ application/config.php
@@ -164,8 +164,8 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__PUBLIC__' => '/jiushouDeclare/public',
-        '__ROOT__' => '/jiushouDeclare'
+        '__PUBLIC__' => '/customs/public',
+        '__ROOT__' => '/customs'
     ],
     // 默认跳转页面对应的模板文件
 //    'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
