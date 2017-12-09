7c0b68691ae200a3da5cb5aa287b554fa3db969f
jack <jack@jiushou.com>
2017-12-09 09:02:44 +0800

    修改配置文件静态资源路径

--- application/config.php
+++ application/config.php
@@ -164,8 +164,8 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__PUBLIC__' => '/jiushouDeclare/public',
-        '__ROOT__' => '/jiushouDeclare'
+        '__PUBLIC__' => '/'.basename(ROOT_PATH) .'/public',
+        '__ROOT__' => '/'.basename(ROOT_PATH)
     ],
     // 默认跳转页面对应的模板文件
 //    'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
